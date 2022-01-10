16d821731357caa3e5c81f2486bd0dfd628e4d3a
CodingWhiteHatJr1 <83186646+CodingWhiteHatJr1@users.noreply.github.com>
2022-01-10 18:50:19 -0500

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,14 +1,14 @@
-Books = localStorage.getItem("player-1");
-Reading = localStorage.getItem("player-2");
+Jason = localStorage.getItem("player-1");
+Piper = localStorage.getItem("player-2");
 
-Books_score = 0;
-Reading_score = 0;
+Jason_score = 0;
+Piper_score = 0;
 
-document.getElementById("Books").innerHTML = Jason + " : ";
-document.getElementById("Reading").innerHTML = Piper + " : ";
+document.getElementById("Jason").innerHTML = Jason + " : ";
+document.getElementById("Piper").innerHTML = Piper + " : ";
 
-document.getElementById("Books_score").innerHTML = Jason_score;
-document.getElementById("Reading_score").innerHTML = Piper_score;
+document.getElementById("Jason_score").innerHTML = Jason_score;
+document.getElementById("Piper_score").innerHTML = Piper_score;
 
 document.getElementById("Question_turn").innerHTML = "Question turn : " + Jason;
 document.getElementById("Answer_turn").innerHTML = "Answer turn : " + Piper;
